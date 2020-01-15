8dd764f5797a58f0c7d6d46253ecd7e6affe90e9
Khurliman <kaljkhurliman@gmail.com>
2020-01-16 04:12:13 +0500

    final

---
+++ script.js
@@ -0,0 +1,23 @@
+var strName = prompt('Введите своё имя')
+console.log('Меня зовут ' + strName);
+
+var numAge = +prompt('Ваш возраст')
+console.log('Мне ' + numAge);
+
+var summ = +prompt('Сколько будет 15+3 ?')
+console.log('Пример 1: 15+3=18. Ваш ответ ' + summ);
+
+var minus = +prompt('Сколько будет 26-10 ?')
+console.log('Пример 2: 26-10=16. Ваш ответ ' + minus);
+
+var multi = +prompt('Сколько будет 12*3 ?')
+console.log('Пример 3: 12*3=36. Ваш ответ ' + multi);
+
+var division = +prompt('Сколько будет 15/3 ?')
+console.log('Пример 4: 15/3=5. Ваш ответ ' + division);
+
+var remain = +prompt('Сколько будет 17%3 (т.е. остаток от деления) ?')
+console.log('Пример 5: 17%3=2. Ваш ответ ' + remain);
+
+var open = 'Нажмите Ctrl+Shift+I и откройте консольное окно'
+alert(open);
